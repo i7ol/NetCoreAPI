b69b4025db3e2cc4d4c16a09f8937544f5530279
i7ol <wuocan000@gmail.com>
2025-05-12 02:27:52 +0700

    Update_AssignRole

--- MvcMovie/obj/Debug/net9.0/MvcMovie.AssemblyInfo.cs
+++ MvcMovie/obj/Debug/net9.0/MvcMovie.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b8d2455d35f52d6bcdfcb225b43458aa5d78b5cc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a373f15b7fe83990a3bc73c8791c04db8b526fb0")]
 [assembly: System.Reflection.AssemblyProductAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
